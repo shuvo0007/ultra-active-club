5c567e3d755dd5e2cdbb9f9fa0710653e549004d
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-09-29 02:28:35 +0600

    activity section design complete

--- src/components/activity/Activity.js
+++ src/components/activity/Activity.js
@@ -2,42 +2,58 @@ import React from "react";
 
 const activity = () => {
   return (
-    <div className="bg-white h-full text-left text-2xl p-5">
-      <div>
-        <img src="" alt="" />
-        <h2>Md. Samiul Islam Shuvo</h2>
-        <p> Dhanmondi, Dhaka</p>
-      </div>
-
-      <div>
+    <div className="bg-white h-full text-left text-2xl p-10">
+      <div className="flex mb-5">
+        <img src="img.jpg" alt="pro pic" className="w-14 rounded-xl m-2" />
         <div>
-          <p>65kg</p>
-          <p>Weight</p>
-        </div>
-        <div>
-          <p>5.7</p>
-          <p>height</p>
-        </div>
-        <div>
-          <p>27yrs</p>
-          <p>Age</p>
+          <h2 className="font-bold">Md Samiul Islam</h2>
+          <p> Dhanmondi, Dhaka</p>
         </div>
       </div>
 
-      <p>Add A Break</p>
-
-      <div>
-        <button>10s</button>
-        <button>20s</button>
-        <button>30s</button>
-        <button>40s</button>
-        <button>50s</button>
+      <div className="flex my-5 bg-slate-100 rounded-md justify-evenly">
+        <div className="m-5">
+          <p className="font-bold">65kg</p>
+          <p className="text-gray-400">Weight</p>
+        </div>
+        <div className="m-5">
+          <p className="font-bold">5.7</p>
+          <p className="text-gray-400">height</p>
+        </div>
+        <div className="m-5">
+          <p className="font-bold">27yrs</p>
+          <p className="text-gray-400">Age</p>
+        </div>
       </div>
 
-      <p>Exercise Details</p>
+      <p className="font-bold py-5 ">Add A Break</p>
 
-      <p className="">
-        Exercise time
+      <div className="bg-slate-100 justify-center rounded-md my-5">
+        <button className="bg-white rounded-full p-2 my-5 ml-2 hover:bg-blue-600">
+          20s
+        </button>
+        <button className="bg-white rounded-full p-2 my-5 ml-2 hover:bg-blue-600">
+          10s
+        </button>
+        <button className="bg-white rounded-full p-2 my-5 ml-2 hover:bg-blue-600">
+          30s
+        </button>
+        <button className="bg-white rounded-full p-2 my-5 ml-2 hover:bg-blue-600">
+          40s
+        </button>
+        <button className="bg-white rounded-full p-2 my-5 ml-2 hover:bg-blue-600">
+          50s
+        </button>
+      </div>
+
+      <p className="font-bold py-5">Exercise Details</p>
+
+      <p className="bg-slate-100 p-5 rounded-md my-5">
+        <span className="font-bold">Exercise time</span> <span id=""></span>
+        <span className="text-gray-400 float-right">second</span>
+      </p>
+      <p className="bg-slate-100 p-5 rounded-md my-5">
+        <span className="font-bold">Break time</span> <span id=""></span>
         <span className="text-gray-400 float-right">second</span>
       </p>
 
